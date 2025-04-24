9f56ebe7b5d41ce9e13aaf1e09e7b839a5a0d15f
Shiinoqt <damienrebong@outlook.com>
2025-04-24 17:22:18 +0200

    ultimo eser

--- src/ex/eserciziRipasso/condizionali/compress.py
+++ src/ex/eserciziRipasso/condizionali/compress.py
@@ -1,5 +1,16 @@
 def memorizza_file(files: list[int]) -> None:
     spazio_totale_blocchi = 1000  # Spazio totale disponibile in blocchi
     
-    # Inserisci qui il tuo codice
+    blocchi_occupati = 0
+
+    for file in files:
+        if file > spazio_totale_blocchi * 512:
+            print(f'Non è possibile memorizzare il file di {file} byte. Spazio insufficiente.')
+            break
+        
+        else:
+            fileCompressed = file * 0.8
+            blocchi_occupati = round(fileCompressed / 512)
+            spazio_totale_blocchi -= blocchi_occupati
+            print(f'File di {file} byte compresso in {fileCompressed} byte e memorizzato. Blocchi usati: {blocchi_occupati}. Blocchi rimanenti: {spazio_totale_blocchi}.')
     return
