aacb94f75540a845544bc4280b70139e75b8a42f
Shiinoqt <damienrebong@outlook.com>
2025-07-25 12:28:04 +0200

    accademia 5 query

--- src/ex/bd/accademia/query2.sql
+++ src/ex/bd/accademia/query2.sql
@@ -62,8 +62,31 @@ where p.id = ap.persona
     and ap.giorno = anp.giorno;
 
 -- 9 
-select p.id, p.nome, p.cognome, ap.giorno, 
+select p.id, p.nome, p.cognome, ap.giorno, pr.nome prj, ap.oredurata, anp.tipo, anp.oredurata
 from persona p, attivitaprogetto ap, attivitanonprogettuale anp, progetto pr
 where p.id = ap.persona
+    and ap.progetto = pr.id
     and p.id = anp.persona
     and ap.giorno = anp.giorno;
+    
+-- 10
+select p.id, p.nome, p.cognome
+from persona p, attivitaprogetto ap, assenza a
+where p.id = ap.persona
+    and p.id = a.persona
+    and ap.giorno = a.giorno
+
+-- 11
+select p.id, p.nome, p.cognome, ap.giorno, a.tipo causa_ass, pr.nome progetto, ap.oredurata ore_att_prj
+from persona p, attivitaprogetto ap, assenza a, progetto pr
+where p.id = ap.persona
+    and p.id = a.persona
+    and ap.giorno = a.giorno
+    and pr.id = ap.progetto
+
+-- 12
+select wp1.nome
+from wp wp1, wp wp2
+where wp1.nome = wp2.nome
+	and wp1.progetto != wp2.progetto
+	and wp1.progetto < wp2.progetto;
