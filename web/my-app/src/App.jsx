5c5d0d69a2f157e7e60c72e9530242f54ea818cd
Shiinoqt <damienrebong@outlook.com>
2025-09-17 11:55:49 +0200

    eser react reorder

--- web/my-app/src/App.jsx
+++ web/my-app/src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Saluto from './Saluto'
+import CardUtente from './eser2/CardUtente'
+import MenuRistorante from './eser3/MenuRistorante'
+import Termostato from './eser4/Termostato'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+    <div>
     <Saluto />
+    <CardUtente />
+    <MenuRistorante />
+    <Termostato />
+    </div>
   )
 }
 
